4f9c49df82634c41398d31c55ac854c71dc309a4
EgorYakunin <e.yakunin04@gmail.com>
2022-07-09 02:43:33 +0300

    chat messages layout

---
+++ client/src/pages/lk/Chat/Message.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './Message.css'
+
+import MessageText from './MessageText'
+
+function Message(props) {
+    return (
+        <>
+            <div className="--page-container message-container">
+                <div className="message">
+                    <div className="message__avatar">
+                        <h5 className="message__no-avatar --cl">ПВ</h5>
+                    </div>
+
+                    <div className="message__content">
+                        <h3 className="message__author">Пупин Вася</h3>
+
+                        <MessageText />
+                        <MessageText />
+                    </div>
+
+                </div>
+            </div>
+            <hr />
+        </>
+    )
+}
+
+export default Message
---
+++ client/src/pages/lk/Chat/MessageText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MessageText(props) {
+    return (
+        <div className="message__text">
+            <h4 className="message__time">12:54</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, aliquid? Corrupti atque expedita, earum unde corporis itaque in totam quaerat.</p>
+        </div>
+    )
+}
+
+export default MessageText
