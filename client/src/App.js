40a4be3085eb20418cd59a232087c57edcdd6b42
EgorYakunin <e.yakunin04@gmail.com>
2022-08-03 11:55:39 +0300

    react helmet is on

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import {
     BrowserRouter as Router,
@@ -30,54 +31,69 @@ function App() {
     });
 
     return (
-        <Router>
-            <Switch>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/faq" element={<FaqPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-                <Route path="/loyalty" element={<LoyaltyPage />} />
-                <Route path="/login" exact element={<LoginPage />} />
-                <Route path="/register" exact element={<RegisterPage />} />
-                <Route
-                    path="/profile"
-                    exact
-                    element={<Profile user={user} set_user={set_user} />}
-                />
-                <Route path="/my-job" exact element={<MyJob user={user} />} />
-                <Route
-                    path="/reviews"
-                    exact
-                    element={<Reviews user={user} />}
-                />
-                <Route
-                    path="/payments"
-                    exact
-                    element={<Payments user={user} />}
-                />
-                <Route path="/chat" exact element={<Chat user={user} />} />
-                <Route
-                    path="/find-work"
-                    exact
-                    element={<FindWork user={user} />}
-                />
-                <Route
-                    path="/job-offers"
-                    exact
-                    element={<JobOffers user={user} />}
-                />
-                <Route
-                    path="/worker-bank"
-                    exact
-                    element={<WorkerBank user={user} />}
-                />
-                <Route
-                    path="/find-workers"
-                    exact
-                    element={<FindWorkers user={user} />}
-                />
-                <Route path="/points" exact element={<Points user={user} />} />
-            </Switch>
-        </Router>
+        <>
+            <Helmet>
+                <title>hr company</title>
+                <meta name="referrer" content="origin" />
+            </Helmet>
+
+            <Router>
+                <Switch>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/faq" element={<FaqPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
+                    <Route path="/loyalty" element={<LoyaltyPage />} />
+                    <Route path="/login" exact element={<LoginPage />} />
+                    <Route path="/register" exact element={<RegisterPage />} />
+                    <Route
+                        path="/profile"
+                        exact
+                        element={<Profile user={user} set_user={set_user} />}
+                    />
+                    <Route
+                        path="/my-job"
+                        exact
+                        element={<MyJob user={user} />}
+                    />
+                    <Route
+                        path="/reviews"
+                        exact
+                        element={<Reviews user={user} />}
+                    />
+                    <Route
+                        path="/payments"
+                        exact
+                        element={<Payments user={user} />}
+                    />
+                    <Route path="/chat" exact element={<Chat user={user} />} />
+                    <Route
+                        path="/find-work"
+                        exact
+                        element={<FindWork user={user} />}
+                    />
+                    <Route
+                        path="/job-offers"
+                        exact
+                        element={<JobOffers user={user} />}
+                    />
+                    <Route
+                        path="/worker-bank"
+                        exact
+                        element={<WorkerBank user={user} />}
+                    />
+                    <Route
+                        path="/find-workers"
+                        exact
+                        element={<FindWorkers user={user} />}
+                    />
+                    <Route
+                        path="/points"
+                        exact
+                        element={<Points user={user} />}
+                    />
+                </Switch>
+            </Router>
+        </>
     );
 }
 export default App;
