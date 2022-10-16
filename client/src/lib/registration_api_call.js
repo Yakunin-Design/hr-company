a79131f6b5f5a6d9f923752934ce20df7da7b334
Qvezal <andrey.kazako75@gmail.com>
2022-10-16 14:05:02 +0300

    client delete

--- client/src/lib/registration_api_call.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-async function registration_api_call(data) {
-    const response = await axios.post("http://localhost:6969/signup", data)
-
-    return response
-}
-
-export default registration_api_call
