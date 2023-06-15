80f842a17f04432ad7c76e0bba6b74344412e325
EgorYakunin <e.yakunin04@gmail.com>
2023-06-15 20:29:42 +0300

    school manager plate

--- app/(service)/test/page.tsx
+++ app/(service)/test/page.tsx
@@ -4,6 +4,7 @@ import Row from "@/components/std/Row";
 
 import AddressPlate from "@/components/smp/AddressPlate";
 import PositionsIndicator from "@/components/smp/PositionsIndicator";
+import SchoolManagerPlate from "@/components/smp/SchoolManagerPlate";
 
 export default function TestPage() {
     return (
@@ -12,18 +13,19 @@ export default function TestPage() {
 			<h2>Welcome to the test page</h2>
 			<p>for test purposes</p>
 			<Spacer top={2}/>
+
 			<AddressPlate 
 				school="Школа 486" 
 				address="Дворцовая площадь д1"
 				positions={5}
 				available={2}
 			/>
-			<AddressPlate 
-				school="Школа 486" 
-				address="Дворцовая площадь д1"
-				positions={5}
-				available={2}
-			/>
+
+			<SchoolManagerPlate
+				full_name="Смирнов Василий Александрович"
+				phone="+79876543221"
+			/>	
+
 		</Container>
 	)
 }
---
+++ components/smp/SchoolManagerPlate/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Row from "@/components/std/Row";
+import Card from "@/components/Card";
+import styles from "./SchoolManagerPlate.module.css";
+import phone_icon from "./phone.svg";
+
+type props = {
+	full_name: string,
+	phone: string,
+	position?: string
+};
+
+export default function SchoolManagerPlate(props: props) {
+	const tel_link = "tel:" + props.phone;
+
+    return (
+		<a href={tel_link} className={styles.plate}>
+			<Card>
+				<Row>
+					<div>
+						<h3>{props.position ? position : "Заведующий"}</h3>
+						<p>{props.full_name}</p>
+					</div>
+					<Image 
+						src={phone_icon}
+						width={17}
+						height={17}
+						alt="phone icon"
+					/>
+				</Row>
+			</Card>
+		</a>
+	)
+}
