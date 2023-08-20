0bbff122138f036c26cf7257202a2a65cf48514c
Yakunin Egor <e.yakunin04@gmail.com>
2023-08-21 02:23:50 +0300

    proposals draft

--- app/(smp)/proposal/[id]/get_data.ts
+++ app/(smp)/proposal/[id]/get_data.ts
@@ -1,14 +1,22 @@
 import axios from "axios";
+import { cookies } from "next/headers";
 
 export default async function get_data(id: string) {
-    const uri = `${process.env.API_ADDRESS}/smp-job-offers/${id}`;
-    const res = await axios.get(uri);
+    const cookieStore = cookies();
+    const jwt = cookieStore.get("jwt")?.value;
+    const config = {
+        headers: {
+            authorization: "Bearer " + jwt,
+        },
+    };
+
+    const uri = `${process.env.API_ADDRESS}/proposal/${id}`;
+    const res = await axios.get(uri, config);
 
     if (!res) {
         throw new Error("data fetch failed");
     }
 
     const jo_data = res.data;
-
     return jo_data;
 }
